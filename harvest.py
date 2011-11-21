664e152a3298131d2a424a786168bdf5aa56fd22
Russell Duhon <fugu13@gmail.com>
2011-11-21 13:45:33 -0800

    incremental harvesting change. Note: the repository gives far early date for earliest, so there's a lot of nothing to download here churning before it gets to anything, first run

--- harvest.py
+++ harvest.py
@@ -3,25 +3,29 @@
 import sys
 import shelve
 import bsddb
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from decorator import decorator
 from oaipmh.client import Client
 from oaipmh.metadata import MetadataRegistry, oai_dc_reader
+from oaipmh.error import NoRecordsMatchError
 
 
-@decorator
-def memoize(func, *args, **kw):
+def _memoize(func, *args, **kw):
     if kw: # frozenset is used to ensure hashability
         key = args, frozenset(kw.iteritems())
     else:
         key = args
-    cache = func.cache # attributed added by memoize
+    cache = func.cache  # attributed added by memoize
     if key in cache:
         return cache[key]
     else:
         cache[key] = result = func(*args, **kw)
         return result
+
+def memoize(f):
+    f.cache = {}
+    return decorator(_memoize, f)
 
 class Store:
     def __init__(self):
@@ -64,23 +68,36 @@ store = Store()
 
 if len(sys.argv) > 1:
     start = datetime.strptime(sys.argv[1], '%Y-%m-%d') #2011-10-27, for instance
-elif store.last(): #start with the last day in the database
-    start = store.last()
+#elif store.last(): #start with the last day in the database
+#    start = store.last() #NOTE: doesn't work due to not returned in right order
 else:
     start = client.identify().earliestDatestamp()
 
 #try this and see if it works; if it does resumption tokens right, this should work fine.
 
-print >>sys.stderr, "fetching records @", now(), "starting with", start.strftime('%Y-%m-%d')
-records = client.listRecords(metadataPrefix='oai_dc', from_=start)
-print >>sys.stderr, "initial record fetch finished @", now()
+
+chunk = timedelta(days=30)
+
+#TODO: clearly they don't do this whole "ordered" thing. Grab records by month or year or something instead of all at once.
+#TODO: luckily, once we've done a full slurp, we only need to remember when the last full slurp was and start since then. But if interrupted, we need to start back from where the last *full* slurp was, due to the ordering problem.
 
 #TODO: structure this better, with the try effectively moved much further above. Really, move a lot more into functions
 try:
-    for index, (header, metadata, _) in enumerate(records, start=1):
-        store.write_record(header, metadata)
-        if index % 1000 == 0:
-            print >>sys.stderr, "  wrote record", index, "of", header.datestamp().strftime('%Y-%m-%d'), "with id", header.identifier()
+    current = start #TODO: make a nice little generator so I can use a for loop
+    while current <= datetime.now():
+        print >>sys.stderr, "fetching records @", now(), "starting with", current.strftime('%Y-%m-%d')
+        try:
+            records = client.listRecords(metadataPrefix='oai_dc', from_=start, until=(current + chunk))
+        except NoRecordsMatchError:
+            print >>sys.stderr, "no records for this chunk, continuing to next"
+            current += chunk
+            continue
+        print >>sys.stderr, "record fetch finished @", now()
+        for index, (header, metadata, _) in enumerate(records, start=1):
+            store.write_record(header, metadata)
+            if index == 1 or index % 1000 == 0:
+                print >>sys.stderr, "  wrote record", index, "of", header.datestamp().strftime('%Y-%m-%d'), "with id", header.identifier()
+        current += chunk
 finally:
     print >>sys.stderr, "closing store"
     store.close()
